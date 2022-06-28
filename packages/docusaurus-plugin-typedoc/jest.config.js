476032076ae5492d065e453e52c62cb5d69ece7f
chmanie <code@chmanie.com>
2022-06-28 18:12:27 +0200

    Adjust to GitBook and remove other packages

--- packages/docusaurus-plugin-typedoc/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require('../../jest.config.base.js');
-
-module.exports = {
-  ...base,
-  name: 'docusaurus-plugin-typedoc',
-  displayName: 'docusaurus-plugin-typedoc',
-};
--- packages/typedoc-github-wiki-theme/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require('../../jest.config.base.js');
-
-module.exports = {
-  ...base,
-  name: 'typedoc-github-wiki-theme',
-  displayName: 'typedoc-github-wiki-theme',
-};
